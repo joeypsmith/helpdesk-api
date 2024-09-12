63789d799b5fa07418caac6d5e3bb49a65f42caa
josephsmith1029 <75954138+josephsmith1029@users.noreply.github.com>
2024-09-11 21:38:15 -0400

    Added CRUD operation controllers

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     roles: [{
         type: String,
-        default: 'Contact'
+        default: "Contact"
     }],
     active: {
         type: Boolean,
